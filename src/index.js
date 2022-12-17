7ef9faad235083153c9e0e10e5eb51cd415367bd
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2022-12-17 17:21:46 +0400

    adds simple modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
 
-axios
-  .get("https://jsonplaceholder.typicode.com/todos/")
-  .then((res) => console.log(res.data));
+// axios
+//   .get("http://localhost:3000/someData")
+//   .then((res) => console.log(res.data));
+
+// elements
+const openModalButton = document.getElementById("open-modal-button");
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+
+// modal actions
+function openModal() {
+  backdrop.style.display = "block";
+  modal.style.display = "block";
+}
+
+openModalButton.addEventListener("click", openModal);
