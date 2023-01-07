d14288dcd369cadf2d1d7e1f43aa7833df5af952
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2023-01-07 16:42:58 +0400

    adds communication with server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-// import axios from "axios";
+import axios from "axios";
 
-// axios
-//   .get("http://localhost:3000/someData")
-//   .then((res) => console.log(res.data));
+axios.get("http://localhost:3000/list").then((res) => renderList(res.data));
 
 // elements
-const openModalButton = document.getElementById("open-modal-button");
+const list = document.querySelector(".container ul");
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const modalCloseButton = document.querySelector(".modal .modal__top button");
+const textarea = document.querySelector(".modal__main-content textarea");
+const modalCancelButton = document.querySelector(
+  ".modal__bottom__actions-cancel"
+);
+const modalAcceptButton = document.querySelector(
+  ".modal__bottom__actions-accept"
+);
+
+let currentItem;
+let currentItemId;
 
 // modal actions
 function openModal() {
@@ -21,5 +29,47 @@ function closeModal() {
   modal.style.display = "none";
 }
 
-openModalButton.addEventListener("click", openModal);
+function renderList(listData) {
+  listData.forEach((listDataItem) => {
+    const li = document.createElement("li");
+    const h1 = document.createElement("h1");
+    const button = document.createElement("button");
+
+    h1.innerText = listDataItem.title;
+    button.innerText = "edit";
+
+    li.appendChild(h1);
+    li.appendChild(button);
+    li.setAttribute("id", listDataItem.id);
+
+    list.appendChild(li);
+  });
+
+  const listItems = document.querySelectorAll("ul li");
+  listItems.forEach((item) =>
+    item.lastElementChild.addEventListener("click", () =>
+      itemEditClickHandler(item)
+    )
+  );
+}
+
+function itemEditClickHandler(item) {
+  openModal();
+  currentItem = item;
+  currentItemId = item.getAttribute("id");
+  textarea.value = item.firstElementChild.innerText;
+}
+
+function modalAcceptClickHandler() {
+  currentItem.firstElementChild.innerText = textarea.value;
+
+  axios.put(`http://localhost:3000/list/${currentItemId}`, {
+    id: 1,
+    title: textarea.value,
+  });
+  closeModal();
+}
+
 modalCloseButton.addEventListener("click", closeModal);
+modalCancelButton.addEventListener("click", closeModal);
+modalAcceptButton.addEventListener("click", modalAcceptClickHandler);
